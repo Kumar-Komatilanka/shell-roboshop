037dd030f820e024f8b45d03acb81f652d6f0a73
Kumar <ksitakumar2@gmail.com>
2026-02-10 16:59:20 +0530

    adding colours to our script

--- frontend.sh
+++ frontend.sh
@@ -55,7 +55,7 @@ VALIDATE $? "unzipping frontend"
 rm -rf /etc/nginx/nginx.conf &>>$LOG_File
 VALIDATE $? "Remove default nginx conf"
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+cp $SCRIPT_DIR/nginix.conf /etc/nginx/nginx.conf
 VALIDATE $? "Copying nginx.conf"
 
 systemctl restart nginx 
